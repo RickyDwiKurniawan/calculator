7ae0c1c26a38afef859b203cc0a3bec0c3cdfa65
Ricky Dwi Kurniawan <rickydwi77@outlook.com>
2023-04-04 16:48:55 +0700

    add % operation

--- script.js
+++ script.js
@@ -60,6 +60,9 @@ const calculate = () => {
         case "/":
             result = parseFloat(prevNumber) / parseFloat(currentNumber)
             break;
+        case "%":
+            result = parseFloat(prevNumber) / 100
+            break;
         default:
             return;
     }
