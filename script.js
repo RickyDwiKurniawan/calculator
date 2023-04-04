018423601f0f10a2663e946b11d7973527888371
Ricky Dwi Kurniawan <rickydwi77@outlook.com>
2023-04-04 16:28:54 +0700

    input number & operator

--- script.js
+++ script.js
@@ -8,6 +8,38 @@ const numbers = document.querySelectorAll(".number")
 
 numbers.forEach((number) => {
     number.addEventListener("click", (e) =>{
-        updateScreen(e.target.value)
+        inputNumber(e.target.value)
+        updateScreen(currentNumber)
     })
+})
+
+let prevNumber = ''
+let operation = ''
+let currentNumber = '0'
+
+const inputNumber = (number) => {
+    if(currentNumber === '0'){
+        currentNumber = number
+    }else{
+        currentNumber += number
+    }  
+}
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) =>{
+    operator.addEventListener("click", (e) => {
+        inputOperator(e.target.value)
+    })
+})
+
+const inputOperator = (operator) =>{
+    prevNumber = currentNumber
+    operation = operator
+    currentNumber = ''
+}
+const equalSign = document.querySelector(".equal-sign")
+
+equalSign.addEventListener("click", () => {
+    console.log('equal button is pressed')
 })
